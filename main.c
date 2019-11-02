1c5a84f148b324a4343cd2314d034529c2c89a57
Ximelele <druzbacky.martin@gmail.com>
2019-11-02 23:55:07 +0100

    Small steps

--- main.c
+++ main.c
@@ -5,22 +5,26 @@
 int main() {
     char povodne[MAX],sifra[MAXD];
     FILE *fr,*fw;
-    int i,c,b;
+    int i,c,counter=0;
     c = getchar();
-while(c!='k') {
-    switch (c) {
-        case 'n' :
-            if ((fr = fopen("1000znakov.txt", "r")) == NULL) {
-                printf("Spravu sa nepodarilo nacitat\n");
+    while(c!='k') {
+        switch (c) {
+            case 'n' :
+                if ((fr = fopen("1000znakov.txt", "r")) == NULL) {
+                    printf("Spravu sa nepodarilo nacitat\n");
 
-            }
-            while ((c = getc(fr)) != EOF) {
-                putchar(c);
-            }
-        case 'v':
-            printf("%c",povodne);
+                }
+                while ((c = getc(fr)) != EOF) {
+                    if((c>= 'a'&& c<='z')||(c>= 'A'&& c<='Z')){
+                        counter++;
+                    }
+                }
+                printf("%d",counter);
+
+
+        }
+        break;
     }
-}
 
     return 0;
 }
