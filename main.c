fdf069c5af6df874a572056e992ea63c35df3fc8
Ximelele <druzbacky.martin@gmail.com>
2019-11-02 23:24:34 +0100

    Improving

--- main.c
+++ main.c
@@ -3,7 +3,22 @@
 #define MAXD 1000
 
 int main() {
-    char original[MAX],crypted[MAXD];
+    char povodne[MAX],sifra[MAXD];
+    FILE *fr,*fw;
+    int i,c,b;
+    c = getchar();
+while(c!='k') {
+    switch (c) {
+        case 'n' :
+            if ((fr = fopen("1000znakov.txt", "r")) == NULL) {
+                printf("Spravu sa nepodarilo nacitat\n");
+                return 0;
+            }
+            while ((b = getc(fr)) != EOF) {
+                putchar(b);
+            }
+    }
+}
 
     return 0;
 }
