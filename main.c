b7a6e60ccaf047a237e628df11f470e9ba6a605b
Ximelele <druzbacky.martin@gmail.com>
2019-11-05 21:45:49 +0100

    Pridanie funkce pre U

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-
+#include <ctype.h>
 #define MAX 1000
-#define MAXD 1000
+#define SUBOR "sifra.txt"
+
+int ABC(char povodne[], char sifra[])
+{
+    int i,k=0;
+    for (i=0;i< MAX;i++){
+        if((povodne[i]>='A'&&povodne[i]<='Z')||(povodne[i]>='a'&&povodne[i]<='z')){
+            sifra[k]=toupper(povodne[i]);
+            k++;
+        }
+
+    }
+    return k;
+}
+
 
 int main() {
-    char povodne[MAX]="",sifra[MAXD];
+    char povodne[MAX]="",sifra[MAX];
     FILE *fr;
     int c;
 
@@ -12,21 +26,24 @@ int main() {
     while(c!='k') {//pokial c==k
         switch (c = getchar()) {//nacitanie charakteru pre switch
             case 'n' :
-                if ((fr = fopen("1000znakov.txt", "r")) == NULL) {//overenie ci sa subor da otvorit
+                if ((fr = fopen(SUBOR, "r")) == NULL) {//overenie ci sa subor da otvorit
                     printf("Spravu sa nepodarilo nacitat\n");
 
                 }
-
                 for (int i = 0; i < sizeof(povodne); i++) {//for pre rozsah
                     fscanf(fr, "%c", &povodne[i]);//ukladanie do pola
+                }
+                if(fclose(fr) == EOF) {
+                    printf("Chyba: zatvaranie suboru\n");
                 }
                 break;
             case 'v':
                 if(povodne[0]=='\0') {//overenie ci je pole povodny prazdne
-                        printf("Sprava nie je nacitana");
-                        }
+                    printf("Sprava nie je nacitana");
+                }
                 else{
                     for (int i = 0; i < sizeof(povodne); i++) {
+                        if(povodne[i]!='\0')
                         printf("%c", povodne[i]);//skusobny vypis
                     }
                 }
@@ -34,11 +51,23 @@ int main() {
                 break;
             case 'u':
                 if(povodne[0]=='\0') {//overenie ci je pole povodny prazdne
-                    printf("Sprava nie je nacitana");
+                    printf("Sprava nie je nacitana\n");
                 }
+                int ucase = ABC(povodne, sifra);
+                for (int i = 0; i < ucase; i++)
+                {
+                    if (i > 0)
+                        printf("");
+                    printf("%c", sifra[i]);
+                }
+                printf("\n");
+                break;
         }
-    }
 
-fclose(fr);
+
+        }
+
+
+    fclose(fr);
     return 0;
 }
